cb14c98e4075ff0cd096feef322a3425b10b2b54
Shea Levy <shea.levy@zalora.com>
2014-02-06 13:45:41 -0500

    There is now a buildLocalCabal function

--- default.nix
+++ default.nix
@@ -1,26 +1,10 @@
 { nixpkgs ? <nixpkgs>
 , src ? ./. # Eventually want to filter out ignores
-, officialRelease ? false
+, zalora-nix-lib ? <zalora-nix-lib>
 }:
 let
+  nix-lib = import zalora-nix-lib { inherit pkgs; };
   pkgs = import nixpkgs {};
-
-  inherit (pkgs) haskellPackages lib;
-
-  # Following to be moved into a library once we build more than
-  # one package with nix
-  cabalExpr = pkgs.runCommand "sproxy.nix" {} ''
-    ${haskellPackages.cabal2nix}/bin/cabal2nix ${src + "/sproxy.cabal"} \
-      --sha256=FILTERME | grep -v FILTERME > $out
-  '';
-
-  versionSuffix = if officialRelease
-    then ""
-    else "pre-${src.shortRev or "git"}";
-in
-  lib.overrideDerivation (haskellPackages.callPackage cabalExpr {
-    tls = pkgs.haskellPackages.tls_1_1_5;
-  }) (self: {
-    name = self.name + versionSuffix;
-    inherit src;
-  })
+in (nix-lib.buildLocalCabal src "sproxy").override {
+  tls = pkgs.haskellPackages.tls_1_1_5;
+}
