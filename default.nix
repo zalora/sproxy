bf9b436639e24bb0f3401cf030f5aa7ac27a3d5d
Shea Levy <shea.levy@zalora.com>
2014-02-11 11:20:30 -0500

    Remove unneeded override

--- default.nix
+++ default.nix
@@ -1,6 +1,4 @@
 { pkgs ? import <nixpkgs> {}
 , src ? ./. # Eventually want to filter out ignores
 , zalora-nix-lib ? import <zalora-nix-lib> { inherit pkgs; }
-}: (zalora-nix-lib.buildLocalCabal src "sproxy").override {
-  tls = pkgs.haskellPackages.tls_1_1_5;
-}
+}: zalora-nix-lib.buildLocalCabal src "sproxy"
