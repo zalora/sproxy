f7131ff516f8e740ee50e4dff0b7847487736bc0
vi <noreply@example.org>
2014-04-03 14:02:13 +0800

    Portability: use nixpkgs reference to buildLocalCabal, rather than zalora-nix-lib.

--- default.nix
+++ default.nix
@@ -1,8 +1,7 @@
 { pkgs ? import <nixpkgs> {}
 , src ? ./. # Eventually want to filter out ignores
-, zalora-lib ? import <zalora-nix-lib> { inherit pkgs; }
 }:
 
 {
-  build = zalora-lib.buildLocalCabal src "sproxy";
+  build = pkgs.haskellPackages.buildLocalCabal src "sproxy";
 }
