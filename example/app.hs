e13c26393fb5144686520880c07ca9fa3a072948
Simon Hengel <sol@typeful.net>
2014-04-08 19:15:18 +0800

    Add documentation for development setup

---
+++ example/app.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main (main) where
+
+import Network.Wai
+import Network.HTTP.Types
+import Network.Wai.Handler.Warp (run)
+
+app :: Application
+app _ = do
+  return $ responseLBS status200 [("Content-Type", "text/plain")] "hello"
+
+main :: IO ()
+main = run 8080 app
