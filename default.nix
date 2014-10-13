3e3999d6d2047b37d4bce4c699e2784991525112
Vladimir Kirillov <proger@wilab.org.ua>
2014-10-13 13:43:05 +0300

    default.nix: provide buildExecutableOnly

--- default.nix
+++ default.nix
@@ -2,15 +2,27 @@
 , src ? ./. # Eventually want to filter out ignores
 }:
 
-{
+let wrap = ''
+      source ${pkgs.makeWrapper}/nix-support/setup-hook
+      wrapProgram $out/bin/sproxy \
+      --prefix LD_LIBRARY_PATH : ${pkgs.stdenv.gcc.gcc}/lib64
+    '';
+in {
   build = pkgs.haskellPackages.buildLocalCabalWithArgs {
     inherit src;
     name = "sproxy";
     cabalDrvArgs = {
+        postInstall = wrap;
+    };
+  };
+
+  buildExecutableOnly = pkgs.haskellPackages.buildLocalCabalWithArgs {
+    inherit src;
+    name = "sproxy";
+    cabalDrvArgs = {
         postInstall = ''
-            source ${pkgs.makeWrapper}/nix-support/setup-hook
-            wrapProgram $out/bin/sproxy \
-            --prefix LD_LIBRARY_PATH : ${pkgs.stdenv.gcc.gcc}/lib64
+            rm -rf $out/lib $out/share/doc
+            ${wrap}
         '';
     };
   };
