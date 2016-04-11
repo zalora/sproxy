a9d527e3fe87711bc1167eb658aae949325d5149
Igor Pashev <pashev.igor@gmail.com>
2016-04-12 02:24:01 +0800

    Use docopt

--- src/Main.hs
+++ src/Main.hs
@@ -1,25 +1,43 @@
+{-# LANGUAGE QuasiQuotes #-}
 module Main
 (
   main
 ) where
 
-import Options.Applicative
+import Data.Maybe (fromJust)
+import Data.Version (showVersion)
+import Paths_sproxy (version) -- from cabal
+import System.Environment (getArgs)
+import Text.RawString.QQ (r)
+import qualified System.Console.Docopt.NoTH as O
 
 import Authorize (withDatabaseAuthorizeAction)
 import ConfigFile (withConfigFile, ConfigFile(..))
 import Proxy (run)
 
+usage :: String
+usage =  "SProxy " ++ showVersion version ++
+  " HTTP proxy for authenticating users via Google OAuth2" ++ [r|
+
+Usage:
+  sproxy [options]
+
+Options:
+  -c, --config=FILE        Configuration file [default: config/sproxy.yml]
+  -h, --help               Show this message
+
+|]
+
 main :: IO ()
 main = do
-  configFile <- execParser options
-  withConfigFile configFile $ \config ->
-    withDatabaseAuthorizeAction (cfDatabase config) (run config)
-  where
-    parser = strOption (
-         long "config"
-      <> noArgError ShowHelpText
-      <> metavar "CONFIG"
-      <> value "config/sproxy.yml"
-      <> help "config file path"
-      )
-    options = info parser (fullDesc <> progDesc "sproxy: proxy for single sign-on")
+  doco <- O.parseUsageOrExit usage
+  args <- O.parseArgsOrExit doco =<< getArgs
+  if args `O.isPresent` O.longOption "help"
+  then putStrLn $ O.usage doco
+  else do
+    let
+      configFile = fromJust . O.getArg args $ O.longOption "config"
+
+    withConfigFile configFile $ \config ->
+      withDatabaseAuthorizeAction (cfDatabase config) (run config)
+
